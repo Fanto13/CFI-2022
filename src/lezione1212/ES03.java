0aff5e56c0e674fdd4b6c546eefb7f03b939a859
JCobot <mircobotti238@gmail.com>
2022-12-13 09:31:44 +0100

    lezione1212
    Nella lezione si sono affrontati diversi esercizi su for e while utilizzando gli array

---
+++ src/lezione1212/ES03.java
@@ -0,0 +1,57 @@
+package lezione1212;
+
+public class ES03 {
+
+    static void stampaArray(int[] v) {
+        for(int i = 0; i < v.length; i++) {
+            System.out.println("Elemento " + i + ": " + v[i]);
+        }
+    }
+
+    static void inizializzaArrayRandom(int[] v) {
+        for (int i = 0; i < v.length; i++) v[i] = (int)(Math.random()*1000);
+
+        /*
+        int i = 0;
+        while(i < v.length) {
+            v[i] = (int)(Math.random()*1000);
+
+            i++;
+        }
+         */
+    }
+
+    static boolean isSorted(int[] v) { // in maniera crescente
+        // l'ordine viene dato per scontato
+        boolean sorted = true;
+        for(int i = 0; i < v.length - 1; i++) {
+            if(v[i] > v[i+1]) {
+                // un solo elemento fuori posto interrompe il tutto
+                sorted = false;
+                break;
+            }
+        }
+        return sorted;
+    }
+
+    public static void main(String[] args) {
+        int v[] = new int[10];
+
+        inizializzaArrayRandom(v);
+
+
+        for (int i = 0; i < v.length - 1; i++) {     // per ogni elemento, ultimo escluso
+            for (int j = i + 1; j < v.length; j++) { // lo confronto con tutti gli elementi dopo di lui
+                                                     // dando per scontato che i precedenti siano già ordinati
+                if(v[i] > v[j]) {
+                    int tmp = v[i];
+                    v[i] = v[j];
+                    v[j] = tmp;
+                }
+            }
+        }
+
+
+        stampaArray(v);
+    }
+}
