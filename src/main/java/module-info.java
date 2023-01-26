e994dae0ac9ad36386d7e5c039257fb8f022cffa
Nicola Bicocchi <nbicocchi@unimore.it>
2023-01-26 11:34:03 +0100

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,16 @@
 module com.example.tempconverter {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires org.controlsfx.controls;
 
     opens com.example.tempconverter to javafx.fxml;
     opens com.example.divider to javafx.fxml;
     opens com.example.planes to javafx.fxml;
+    opens com.example.paint to javafx.graphics;
 
     exports com.example.tempconverter;
     exports com.example.divider;
     exports com.example.planes;
+    exports com.example.paint;
 }
