9f21d2a5a839f7bf7b88ef7797a396ee1ef48159
Nicola Bicocchi <nbicocchi@unimore.it>
2023-02-02 14:49:16 +0100

    update

---
+++ src/main/java/com/example/moneymanager/LoginController.java
@@ -0,0 +1,21 @@
+package com.example.moneymanager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private PasswordField tfPassword;
+
+    @FXML
+    private TextField tfUsername;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
+
+}
