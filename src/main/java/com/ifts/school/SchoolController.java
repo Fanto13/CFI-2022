f326f8b2b65d0373c60ba9c5e633b29b1d2ed6cf
Nicola Bicocchi <nbicocchi@unimore.it>
2023-02-14 11:59:00 +0100

    update

---
+++ src/main/java/com/ifts/school/SchoolController.java
@@ -0,0 +1,28 @@
+package com.ifts.school;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SchoolController {
+    @FXML
+    void onClasses() {
+
+    }
+
+    @FXML
+    void onDashboard() {
+
+    }
+
+    @FXML
+    void onStudents() {
+
+    }
+
+}
