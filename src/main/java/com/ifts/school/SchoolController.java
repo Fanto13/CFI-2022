b2f6c78b70c595e2e16b5a761a857a05626b0452
Nicola Bicocchi <nbicocchi@unimore.it>
2023-02-14 12:25:04 +0100

    update

--- src/main/java/com/ifts/school/SchoolController.java
+++ src/main/java/com/ifts/school/SchoolController.java
@@ -5,24 +5,35 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SchoolController {
     @FXML
-    void onClasses() {
+    private AnchorPane rightPane;
 
+    @FXML
+    void onClasses() throws IOException {
+        switchPanel("classes-view.fxml");
     }
 
     @FXML
-    void onDashboard() {
-
+    void onDashboard() throws IOException {
+        switchPanel("dashboard-view.fxml");
     }
 
     @FXML
-    void onStudents() {
-
+    void onStudents() throws IOException {
+        switchPanel("students-view.fxml");
     }
 
+    void switchPanel(String view) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(view));
+        Parent pane = loader.load();
+        rightPane.getChildren().clear();
+        rightPane.getChildren().add(pane);
+    }
 }
